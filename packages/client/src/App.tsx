a6d020293d7cbf481763fe562b03dd0c13dfe840
danny huang <dannyhuang1234@hotmail.com>
2022-01-25 00:04:17 +1100

    WIP got lazy query going, TODO put in grid table with all the details

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -3,9 +3,9 @@ import type { FC } from 'react'
 import { ChakraProvider, Box, extendTheme } from '@chakra-ui/react'
 import { Route, Routes } from 'react-router-dom'
 import { TopBar } from './TopBar'
-import { Home } from './Home'
-import { WishList } from './WishList'
-import { Visited } from './Visited'
+import { Home } from './components/Home/Home'
+import { WishList } from './components/WishList/WishList'
+import { Visited } from './components/Visited/Visited'
 
 const fonts = {
   heading:
--- packages/client/src/components/Home/Home.tsx
+++ packages/client/src/components/Home/Home.tsx
@@ -2,29 +2,37 @@ import { Container, Heading, VStack } from '@chakra-ui/react'
 import { FC, useEffect } from 'react'
 import React, { useState } from 'react'
 import FormInput from '../FormInput/FormInput'
-import { useQuery } from '@apollo/client'
+import getAddress from '../../data/queries/getAddress'
+import { useLazyQuery } from '@apollo/client'
 
 export const Home: FC = () => {
   const [address, setAddress] = useState()
+  const [queryAddress, { loading, error, data }] = useLazyQuery(getAddress)
 
   const handleSubmit = (e: any) => {
     setAddress(e.target[0].value)
+    queryAddress({
+      variables: {
+        filter: {
+          name: e.target[0].value,
+        },
+      },
+    })
+
     e.preventDefault()
   }
 
   useEffect(() => {
-    if (address) {
-      console.log(address)
-      // const { loading, data, error } = useQuery();
+    if (data) {
+      console.log('data', data)
     }
-  }, [address])
+  }, [data])
 
   return (
     <VStack spacing="8">
       <Heading as="h1">Smart traveller</Heading>
       <Container maxW="container.md">
         <FormInput onSubmit={handleSubmit} />
-        <
       </Container>
     </VStack>
   )
