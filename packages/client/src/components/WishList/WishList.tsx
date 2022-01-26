43c63d3093fd9762d4bd5899b816851f0c2439d1
danny huang <dannyhuang1234@hotmail.com>
2022-01-26 18:18:26 +1100

    done with displaying visited/wishlist to true, TODO UT

--- packages/client/src/components/WishList/WishList.tsx
+++ packages/client/src/components/WishList/WishList.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import { useQuery } from '@apollo/client'
+import { Container, Heading, Spinner } from '@chakra-ui/react'
 import type { FC } from 'react'
-import { Container, Heading } from '@chakra-ui/react'
+import React from 'react'
+import getAddress from '../../data/queries/getAddress'
+import FormTable from '../FormTable/FormTable'
 
-export const WishList: FC = () => (
-  <>
-    <Heading as="h1">Wish list</Heading>
-    <Container centerContent maxW="container.md" flexDir="row"></Container>
-  </>
-)
+export const WishList: FC = () => {
+  const { loading, error, data } = useQuery(getAddress, {
+    variables: {
+      filter: {
+        wishlist: true,
+      },
+    },
+    fetchPolicy: 'network-only', //ensure we pull from DB, not cache
+  })
+
+  if (error) {
+    return <p>an error has occured</p>
+  }
+  return (
+    <>
+      <Heading as="h1">Wish list</Heading>
+      {loading ? <Spinner /> : <FormTable data={data?.cities.cities} total={data?.cities.total} />}
+
+      <Container centerContent maxW="container.md" flexDir="row"></Container>
+    </>
+  )
+}
